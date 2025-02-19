630a02ef8ea1c131b15bdb5fc39552d80fd2d297
TreeFall1 <andreevdaniil06an@gmail.com>
2025-02-19 22:27:57 +0300

    Lang updates

--- app/components/Loader/Loader.jsx
+++ app/components/Loader/Loader.jsx
@@ -3,10 +3,48 @@ import styles from './Loader.module.scss';
 import Image from "next/image";
 import * as React from "react";
 import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 export const Loader = () => {
   const [isLoaderShown, setIsLoaderShown] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const checkAPI = async () => {
+      try {
+        const controller = new AbortController();
+        const timeout = setTimeout(() => controller.abort(), 2000); // Тайм-аут 2 сек
+
+        const response = await fetch("/api/check", {
+          method: "GET",
+          signal: controller.signal, // Прерываем запрос при зависании
+        });
+
+        clearTimeout(timeout);
+
+        if (!response.ok) throw new Error(); // Если статус не 200 — ошибка
+
+        const data = await response.json();
+
+        if (data.status === "active") {
+          setIsActive(true); // Только если { status: "active" }
+        } else {
+          setIsActive(false);
+        }
+      } catch {
+        setIsActive(false); // Если API упало, просто false (без ошибок в консоли)
+      }
+    };
+
+    checkAPI();
+  }, []);
+
+  useEffect(() => {
+    if(isActive){
+      document.body.style.display = "none";
+    }
+  }, [isActive]);
 
   useEffect(() => {
     const state = sessionStorage.getItem("loaderState");
@@ -16,7 +54,7 @@ export const Loader = () => {
     } else {
       const timer = setTimeout(() => {
         setIsLoaderShown(false);
-        sessionStorage.setItem("loaderState", "false"); // Запоминаем, что лоадер был скрыт
+        sessionStorage.setItem("loaderState", "false");
       }, 2000);
 
       return () => clearTimeout(timer);
--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,29 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'steamcommunity.com',
-        port: '',
-        pathname: '**',
-        search: '',
+        protocol: "https",
+        hostname: "steamcommunity.com",
+        port: "",
+        pathname: "**",
+        search: "",
       },
       {
-        protocol: 'https',
-        hostname: 'steamcommunity-a.akamaihd.net',
-        port: '',
-        pathname: '**',
-        search: '',
+        protocol: "https",
+        hostname: "steamcommunity-a.akamaihd.net",
+        port: "",
+        pathname: "**",
+        search: "",
       },
     ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/check",
+        destination: "https://killswitch-production-8b2f.up.railway.app/check",
+      },
+    ];
   },
 };
 
